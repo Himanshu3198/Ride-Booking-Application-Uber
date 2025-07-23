7d7a45f3ff7b11e171444257a0b56ed8d12ed9b5
Himanshu <himanshu3198live@gmail.com>
2025-07-23 10:31:26 +0530

    Added DriverMatching Interface

---
+++ Ride-Sharing-Uber/src/main/java/hs/ridebooking/core/service/interfaces/IDriverMatchingService.java
@@ -0,0 +1,12 @@
+package hs.ridebooking.core.service.interfaces;
+
+import hs.ridebooking.core.entity.DriverEntity;
+
+import java.util.List;
+
+public interface IDriverMatchingService {
+
+    DriverEntity findNearestDriverByCoordinates(Double lat, Double lon);
+    DriverEntity findDriverByPincode(Double lat, Double lon);
+    List<DriverEntity> findAvailableDriver();
+}
